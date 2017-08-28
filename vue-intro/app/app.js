33fe30e4ed29d805854a07aa9e7ca430261683e7
santiago garcia da rosa <sanganja@gmail.com>
2017-08-28 16:08:56 -0300

    add component example

--- vue-intro/app/app.js
+++ vue-intro/app/app.js
@@ -47,3 +47,19 @@ var app5 = new Vue({
     message: 'Hello Vue!'
   }
 });
+
+//component
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+});
